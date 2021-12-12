cf2b11ec1de0b97780504d22ce767ac73f749855
bocon <tan.truong@go2joy.vn>
2021-12-12 09:22:16 +0700

    Update - js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,5 @@
-/*!
- * Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
- * Copyright 2013-2021 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
- */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-//Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
@@ -21,8 +12,13 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
+
+$(document).ready(function () {
+  $("#btnAdd").click(function () {
+    $("#alertBox").show();
+  });
+});
